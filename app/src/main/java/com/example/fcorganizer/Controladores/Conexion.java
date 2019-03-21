baaaf12de463f130da766c2e9c88f09f3e963ecb
PTMARKETING01 <uweb@megasur.es>
2019-03-21 17:35:56 +0100

    Fondo añadido, iniciado AsyncTask en MainActivity

---
+++ app/src/main/java/com/example/fcorganizer/Controladores/Conexion.java
@@ -0,0 +1,10 @@
+package com.example.fcorganizer.Controladores;
+
+import android.os.AsyncTask;
+
+public class Conexion extends AsyncTask{
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
--- app/src/main/java/com/example/fcorganizer/MainActivity.kt
+++ app/src/main/java/com/example/fcorganizer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fcorganizer
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
@@ -8,5 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AsyncTask.execute(Runnable {
+
+        })
     }
 }
