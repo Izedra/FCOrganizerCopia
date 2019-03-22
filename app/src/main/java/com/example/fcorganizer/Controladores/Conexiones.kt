3eabdffe395baee3e387538bc2f68d25acdbc72f
PTMARKETING01 <uweb@megasur.es>
2019-03-22 12:50:02 +0100

    Creado objeto PersonajeP (busqueda principal)

---
+++ app/src/main/java/com/example/fcorganizer/Controladores/Conexiones.kt
@@ -0,0 +1,12 @@
+package com.example.fcorganizer.Controladores
+
+import java.net.URL
+
+class Conexiones(
+    private var url: String = "https://xivapi.com/",
+    private var key: String = "?key=779c4437a51f43a5bb6dcd34"
+){
+    fun getPersonaje(){
+
+    }
+}
